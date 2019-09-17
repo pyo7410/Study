2ca76c9dd34451f87c39804fcce07d9ad54450d9
JSP <jsp7450@gmail.com>
2019-09-17 23:08:26 +0900

    Add stack related algorithm problem
    Add 1406_Editor

---
+++ 백준/1406_Editor/1406_Editor/1406_Editor.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	string str;
+	char cmd;
+	int num, length;
+	stack<char> l_s, r_s;
+
+	cin >> str;
+	length = str.size();
+
+	for (char ch : str)
+	{
+		l_s.push(ch);
+	}
+
+	cin >> num;
+	while (num--)
+	{
+		cin >> cmd;
+
+		if (cmd == 'P')
+		{
+			cin >> cmd;
+			l_s.push(cmd);
+		}
+		else if (cmd == 'B')
+		{
+			l_s.pop();
+		}
+		else if (cmd == 'D')
+		{
+			l_s.push(r_s.top());
+			r_s.pop();
+		}
+		else if (cmd == 'L')
+		{
+			r_s.push(l_s.top());
+			l_s.pop();
+		}
+	}
+
+	while (!l_s.empty())
+	{
+		r_s.push(l_s.top());
+		l_s.pop();
+	}
+
+	while (!r_s.empty())
+	{
+		cout << r_s.top();
+		r_s.pop();
+	}
+	cout << "\n";
+}
