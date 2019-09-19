d15cfa00fe453418a3eb6d79ae1694c2c086bbd0
JSP <jsp7450@gmail.com>
2019-09-19 22:08:49 +0900

    Add stack related algorithm
    Add 17413_Flip_Word_2

---
+++ 백준/17413_Flip_Word_2/17413_Flip_Word_2/Flip_Word_2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+void print(stack<char>& s)
+{
+	while (!s.empty())
+	{
+		cout << s.top();
+		s.pop();
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	stack<char> s;
+	string str;
+	bool tag = false;
+
+	getline(cin, str);
+
+	for (char ch : str)
+	{
+		if (ch == '<')
+		{
+			print(s);
+			tag = true;
+			cout << ch;
+		}
+		else if (ch == '>')
+		{
+			tag = false;
+			cout << ch;
+		}
+		else if (tag)
+		{
+			cout << ch;
+		}
+		else
+		{
+			if (ch == ' ')
+			{
+				print(s);
+				cout << ch;
+			}
+			else
+			{
+				s.push(ch);
+			}
+		}
+	}
+	print(s);
+	cout << "\n";
+}
