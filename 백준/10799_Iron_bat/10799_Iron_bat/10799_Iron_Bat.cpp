da5530cca19970667757ccccbbd185e2d85a1996
JSP <jsp7450@gmail.com>
2019-09-19 22:18:08 +0900

    Add stack related algorithm
    Finished 10799_Iron_Bat

--- 백준/10799_Iron_bat/10799_Iron_bat/10799_Iron_Bat.cpp
+++ 백준/10799_Iron_bat/10799_Iron_bat/10799_Iron_Bat.cpp
@@ -5,5 +5,37 @@ using namespace std;
 
 int main()
 {
-	int cnt;
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	stack<int> s;
+	string str;
+	int result = 0; int idx = 0;
+
+	cin >> str;
+
+	for (char ch : str)
+	{
+		if (ch == '(')
+		{
+			s.push(idx);
+			idx += 1;
+		}
+		if (ch == ')')
+		{
+			if ((idx - s.top()) == 1)
+			{
+				s.pop();
+				result += s.size();
+			}
+			else
+			{
+				s.pop();
+				result += 1;
+			}
+		}
+	}
+
+	cout << result << "\n";
 }
