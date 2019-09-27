17897514854499fdb1969108ee224b45f1234a69
JSP <jsp7450@gmail.com>
2019-09-27 19:46:13 +0900

    Modification 10844_Easy_Staircase
    Add Top-Down

--- 백준/10844_Easy_Staircase/10844_Easy_Staircase/Easy_Staircase.cpp
+++ 백준/10844_Easy_Staircase/10844_Easy_Staircase/Easy_Staircase.cpp
@@ -4,6 +4,42 @@ using namespace std;
 long long d[101][10];
 long long mod = 1000000000LL;
 
+// top-down
+int staircase(int n, int i)
+{
+	if (n == 1)
+	{
+		if (i == 0)
+		{
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+
+	if (d[n][i] > 0)
+	{
+		return d[n][i];
+	}
+
+	//d[n][i] = staircase(n - 1, i - 1) + staircase(n - 1, i + 1);
+
+	if (i - 1 >= 0)
+	{
+		d[n][i] += staircase(n - 1, i - 1);
+	}
+	if (i + 1 <= 9)
+	{
+		d[n][i] += staircase(n - 1, i + 1);
+	}
+
+	d[n][i] %= mod;
+
+	return d[n][i];
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -15,6 +51,8 @@ int main()
 
 	cin >> n;
 
+	// Bottom-Up
+	/*
 	for (int i = 1; i <= 9; ++i)
 	{
 		d[1][i] = 1;
@@ -39,6 +77,15 @@ int main()
 	for (int i = 0; i <= 9; ++i)
 	{
 		result += d[n][i];
+	}
+
+	cout << result % mod << "\n";
+	*/
+
+	// top-down
+	for (int i = 0; i <= 9; ++i)
+	{
+		result += staircase(n, i);
 	}
 
 	cout << result % mod << "\n";
