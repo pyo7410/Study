4e892419b73ae9f796e6922162d722940b682caa
pyo_home <pyo7410@gmail.com>
2020-04-22 00:24:26 +0900

    Bruteforce related algorithm add

---
+++ baek/10973_Prev_permutation/10973_Prev_permutation_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+    int n;
+    int a[10000];
+    cin >> n;
+
+    // 배열의 인덱스 주의
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+
+    if (prev_permutation(a, a + n))
+    {
+        // 배열의 인덱스 주의
+        for (int i = 0; i < n; ++i)
+        {
+            cout << a[i] << " ";
+        }
+    }
+    else
+    {
+        cout << "-1";
+    }
+    
+    cout << "\n";
+
+}
