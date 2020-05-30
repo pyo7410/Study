90d8fffde0ba9781766c9e8cdba6802238c27848
JSP <jsp7450@gmail.com>
2020-05-30 16:22:12 +0900

    Algorithm add 1339_Word_mathmatics add annotation and Bruteforce related add

--- baek/1339_Word_mathematics/1339_Word_mathematics.cpp
+++ baek/1339_Word_mathematics/1339_Word_mathematics.cpp
@@ -13,6 +13,8 @@ int calc(vector<string> &a, vector<char> &letters, vector<int> &d)
 
     for (int i = 0; i < m; ++i)
     {
+        // alpha 배열에는 각 알파벳의 아스키코드에 맞추어 알파벳의 값이 저장된다..
+        // unique와 sort를 썻기 때문에
         alpha[letters[i]] = d[i];
     }
 
@@ -51,7 +53,8 @@ int main()
 
     sort(letters.begin(), letters.end());
     letters.erase(unique(letters.begin(), letters.end()), letters.end());
-    // unique는 연속된 중복원소를 벡터의 맨 뒤로 보내버린다! -> 중복된 원소가 아닌 연속된 원소임을 주의!
+    // unique는 연속된 중복원소를 벡터의 맨 뒤로 보내버린다! -> 중복된 원소가 아닌 연속된 중복 원소임을 주의!
+    // unique는 반환값으로 벡터에서 중복원소의 첫번째 위치를 반환한다.
     // sort했으므로 중복된 원소는 연속으로 쓰이게 된다!
 
     int m = letters.size();
