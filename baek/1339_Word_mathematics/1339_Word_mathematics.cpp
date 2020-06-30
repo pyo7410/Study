0376514d501a1b059b8b4d748bb6c8a0dc20a2a4
pyo_home <pyo7410@gmail.com>
2020-06-30 22:04:11 +0900

    Revert "add: BOJ 알고리즘 문제 추가"
    This reverts commit dbd3bc6d51b9a0b346ae1ceb2ebab3a2b738f64f.

--- baek/1339_Word_mathematics/1339_Word_mathematics.cpp
+++ baek/1339_Word_mathematics/1339_Word_mathematics.cpp
@@ -1,38 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
 #include <string>
 using namespace std;
 
-// char?? 1??? ??? 2^8 = 256
-// ???? ???? ?????? ???? ?? ?? ???? ??
 char alpha[256];
 
-int calc(vector<string> &str, vector<char> &letters, vector<int> &d)
+int calc(vector<string> &a, vector<char> &letters, vector<int> &d)
 {
+    int m = letters.size();
     int sum = 0;
-    int m = letters.size();    
 
-    // ??? ??? ?? ??? ???? ???? ???? ??
     for (int i = 0; i < m; ++i)
     {
+        // alpha 배열에는 각 알파벳의 아스키코드에 맞추어 알파벳의 값이 저장된다..
+        // unique와 sort를 썻기 때문에
         alpha[letters[i]] = d[i];
     }
 
-    for (string s : str)
+    for (string s : a)
     {
         int now = 0;
-        for (char ch : s)
+        for (char x : s)
         {
-            // for?? ?? 0?? ????? ??
-            // ????? 0?? ???? ? ? ???
-            // ?? 10? ???? ??.
-            now = now * 10 + alpha[ch];
+            now = now * 10 + alpha[x];
         }
         sum += now;
     }
-
     return sum;
 }
 
@@ -42,48 +36,47 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-    int n ;
+    int n;
     cin >> n;
 
-    vector<string> str(n);
+    vector<string> a(n);
     vector<char> letters;
 
     for (int i = 0; i < n; ++i)
     {
-        cin >> str[i];
-
-        for (char ch : str[i])
+        cin >> a[i];
+        for (char x : a[i])
         {
-            letters.push_back(ch);
+            letters.push_back(x);
         }
     }
 
     sort(letters.begin(), letters.end());
-
-    // unique? ??? ??? ????? ???? ??? ??? ???? ????.
     letters.erase(unique(letters.begin(), letters.end()), letters.end());
+    // unique는 연속된 중복원소를 벡터의 맨 뒤로 보내버린다! -> 중복된 원소가 아닌 연속된 중복 원소임을 주의!
+    // unique는 반환값으로 벡터에서 중복원소의 첫번째 위치를 반환한다.
+    // sort했으므로 중복된 원소는 연속으로 쓰이게 된다!
 
-    int ans = 0;
     int m = letters.size();
     vector<int> d;
 
-    // ?? ??? ? ??? ???? ??? ???.
-    for (int i = 9; i > 9 - m; i--)
+    for(int i = 9; i > 9 - m; i--)
     {
         d.push_back(i);
     }
 
     sort(d.begin(), d.end());
 
-    do {
-        int now = calc(str, letters, d);
+    int ans = 0;
+    do
+    {
+        int now = calc(a, letters, d);
         if (ans < now)
         {
             ans = now;
         }
     } while (next_permutation(d.begin(), d.end()));
-
     cout << ans << "\n";
 
-    return 0;    
+    return 0;
 }
