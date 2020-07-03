1db686584bb995b218bb4bb6973fede19d0f706b
pyo_home <pyo7410@gmail.com>
2020-07-04 01:44:45 +0900

    add: SWEA initial commit
    SW Expert Academy 알고리즘 문제 추가

---
+++ SWEA/1204_Mode/1204_Mode.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        int n;
+        cin >> n;
+ 
+        vector<int> score_cnt(101, 0);
+ 
+        for (int i = 0; i < 1000; ++i)
+        {
+            int score;
+            cin >> score;
+ 
+            score_cnt[score] += 1;
+        }
+ 
+        int max = score_cnt[0];
+        int ans = 0;
+        for (int i = 1; i < 101; ++i)
+        {
+            if (max <= score_cnt[i])
+            {
+                max = score_cnt[i];
+                ans = i;
+            }
+        }
+ 
+        cout << "#" << n << " " << ans << "\n";
+    }
+ 
+    return 0;
+}
