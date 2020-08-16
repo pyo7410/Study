c20780242ad25296521620110c6562a054e56f7c
pyo7410 <pyo7410@gmail.com>
2020-08-16 15:23:25 +0900

    add: 16934 주석 추가
    - 각 부분에 대한 주석 추가

--- baek/16934_Game_name/16934_Game_name.cpp
+++ baek/16934_Game_name/16934_Game_name.cpp
@@ -10,7 +10,7 @@ class Trie
 {
 private:
     bool finish;
-    int cnt;
+    int cnt;        // 닉네임 개수 count;
     Trie *next[26]; // 알파벳이 26개 이므로
 
 public:
@@ -68,11 +68,6 @@ public:
         ans += *key;
         int curr = *key - 'a';
 
-        if (finish && next[curr] == NULL)
-        {
-            return NULL;
-        }
-
         if (next[curr] == NULL)
         {
             return NULL;
@@ -103,17 +98,28 @@ int main()
 
         if (!root->find(input.c_str()))
         {
+            // 즉, 한번도 닉네임이 생성된 적이 없다면
+            // 새로만든 닉네임이 기존에 Trie에 저장되어있던 닉네임보다 길더라도
+            // if (next[curr] == NULL)로 인해 NULL이 Return 되므로
+            // Trie 알고리즘에 의해 찾지 못했다고 나온다!
+
+            // 또한 새로만든 닉네임이 기존에 Trie에 저장되어있던 닉네임보다 짧고
+            // 접두사가 되더라도 완전히 같은 닉네임이 아니므로 Trie 알고리즘의 if (*key == '\0')에 의해 NULL 값이 리턴되어
+            // 찾지 못했다고 나온다!
+
+            // 찾지못한 닉네임은 insert를 해준다
             if (nick_cnt == -1)
             {
                 root->insert(input.c_str());
             }
         }
 
+        // 닉네임이 한번만 쓰였다면 그대로
         if (nick_cnt == 1)
         {
             cout << ans << "\n";
         }
-        else
+        else // 닉네임이 한번 이상 쓰였다면 쓰일때 마다 닉네임 뒤에 +1 씩 숫자를 붙여 출력
         {
             cout << ans + to_string(nick_cnt) << "\n";
         }
